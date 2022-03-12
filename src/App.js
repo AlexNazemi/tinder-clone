a93e8d9f8e4e1b392eaa3c0016544e8d37e0e5d0
AlexNazemi <alexandernazemi@gmail.com>
2022-03-12 05:38:54 +0000

    TASK-001 create React app starting files, creat all required pages and establish page routing -AN

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Onboarding from "./pages/Onboarding";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+        // specifies the routes to each page
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/onboarding" element={<Onboarding />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
